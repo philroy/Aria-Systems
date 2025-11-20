103ba9d6fce5c8170c56a52c5dc19ffcd21c6226
philroy <hayden@philroy.com.au>
2025-11-20 20:33:00 +1000

    SocketIO code change

--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, emit
 from ai_engine import AriaAI
 
 app = Flask(__name__, static_folder='../frontend', template_folder='../frontend')
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, host='0.0.0.0, port=8080')
 
 aria = AriaAI()
 
